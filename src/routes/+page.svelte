769c8d105b6c91c375bc0e3d45b96fccdde01c1b
Colin Breeding <colinbreeding@Colins-MacBook-Air.local>
2025-08-21 22:39:20 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import kiwiLogo from '$lib/assets/kiwi_logo.png';
+</script>
+
+<div class="flex items-center gap-1">
+	<img src={kiwiLogo} alt="Kiwi Logo" class="h-7 max-h-7 min-h-7 w-7 max-w-7 min-w-7" />
+	<span class="text-xl font-bold">Kiwi</span>
+</div>
