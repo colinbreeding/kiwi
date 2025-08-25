4f077d7262a80da1dc8b2c9d3f12e6441ba6ee74
Colin <colinmbreeding@gmail.com>
2025-08-24 21:50:25 -0400

    Remove NoteToolbar component from note-editor; update NoteEditor.svelte to eliminate its usage, streamlining the editor's structure.

--- src/lib/components/note-editor/NoteEditor.svelte
+++ src/lib/components/note-editor/NoteEditor.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 	import { SidebarInset } from '$lib/components/ui/sidebar';
 	import { NoteManager } from './note-manager';
-	import { NoteToolbar } from './note-toolbar';
 </script>
 
 <SidebarInset class="!m-4 !mt-0 overflow-hidden !rounded-md border border-border/50 bg-muted/50">
 	<NoteManager />
-	<NoteToolbar />
 </SidebarInset>
--- src/lib/components/note-editor/note-toolbar/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as NoteToolbar } from './NoteToolbar.svelte';
